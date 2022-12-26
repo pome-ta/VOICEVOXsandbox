82f0b970c0116ade1a1c72e24fcb6355117545e5
pome-ta <pometarou93@gmail.com>
2022-12-26 21:48:21 +0900

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,22 @@
-console.log('hoge');
+import { hiraFuda } from './yomifuda.js';
+
+const setRequest = (speakText) =>
+  `https://api.tts.quest/v1/voicevox/?text=${speakText}&speaker=1`;
+
+const callVOX = async (req) => {
+  const res = await fetch(req);
+  const json = await res.json();
+  console.log(json);
+  const zun = new Audio(json.mp3DownloadUrl);
+  zun.play();
+};
+
+const startBtn = document.createElement('div');
+startBtn.addEventListener('click', () => {
+  callVOX(setRequest(hiraFuda[0]));
+});
+
+startBtn.style.width = '45px';
+startBtn.style.height = '45px';
+
+document.body.appendChild(startBtn);
